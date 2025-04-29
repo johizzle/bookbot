b55e0c81f705e5fcc5bb1fd447e46f22a7097ff9
johizzle <e.johanne.eriksson@gmail.com>
2025-04-29 19:10:38 +0200

    Redid the formatting of the output, to sort the characters by ascending order, and make it look better

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, sort_dict
 
 def get_book_text(book_path):
     # returns the content of file as a string
@@ -7,10 +7,18 @@ def get_book_text(book_path):
 
 def main():
     words = get_num_words(get_book_text("books/frankenstein.txt"))
-    characters = get_num_char(get_book_text("books/frankenstein.txt"))
-    print(f"{words} words found in the document")
-    print(f"Character count: {characters}")
+    #returns a list with dictionaries with the key and value names char and num
+    character_list = sort_dict(get_num_char(get_book_text("books/frankenstein.txt")))
+    output = ""
+    for entry in character_list:
+        if entry['char'].isalpha():
+            output += f"{entry['char']}: {entry['num']}\n"
 
-
-
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {words} total words
+--------- Character Count -------
+{output}============= END ===============""")
+    
 main()
--- stats.py
+++ stats.py
@@ -4,10 +4,25 @@ def get_num_words(book_string):
 
 def get_num_char(book_string):
     lower_string = book_string.lower()
-    char_dict ={}
+    char_dict = {}
     for i in lower_string:
         if i in char_dict:
             char_dict[i] += 1
         else:
             char_dict[i] = 1
     return char_dict
+
+def sort_dict(char_dict):
+    #Takes each pair in char_dict and adds to a list (of dictionaries)
+    new_list = []
+    for k, v in char_dict.items():
+        new_list.append({"char": k, "num": v})    
+    
+    #Scoped private function for .sort() method below
+    def sort_on(dict):
+        return dict["num"]
+    
+    #Sorts the list (reversed) based on the key, which is a function object (without parentheses) that 
+    # .sort() uses to sort by (return value)
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
